8dc91d1ed1d60af0061e80e6360491955837a374
Dan Stroot <dan.stroot@gmail.com>
2022-03-26 13:28:13 -0700

    Update missioncontrol.sh

--- defaults/missioncontrol.sh
+++ defaults/missioncontrol.sh
@@ -53,5 +53,5 @@ defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Bottom right screen corner → Mission Control
-defaults write com.apple.dock wvous-br-corner -int 2
+defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
